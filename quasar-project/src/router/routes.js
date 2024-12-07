446718c7256bcca777217b9a6936626ed510b6ff
Sumit Nayak <sumitnayak@Sumits-MacBook-Pro-2.local>
2024-12-07 18:02:53 +0530

    modified routes.js

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '/aboutus', component: () => import('pages/AboutUs.vue') },
-      { path: '/feedback', component: () => import('pages/Feedback.vue') },
+      { path: '/feedback', component: () => import('pages/FeedBack.vue') },
       { path: '/help&support', component: () => import('pages/HelpAndSupport.vue') },
       { path: '/login', component: () => import('pages/LoginPage.vue') },
       { path: '/homepage', component: () => import('pages/HomePage.vue') },
