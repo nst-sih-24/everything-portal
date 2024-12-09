941668e8b352773d64f274478cb95677a98c735c
Krushn Dayshmookh <krushndayshmookh@gmail.com>
2024-12-09 17:46:14 +0530

    fix routes

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -5,27 +5,19 @@ const routes = [
     component: () => import('layouts/DeeLayout.vue'),
     children: [
       { path: '/aboutus', component: () => import('pages/AboutUs.vue') },
-<<<<<<< Updated upstream
-      { path: '/feedback', component: () => import('src/pages/FeedBake.vue') },
-=======
       { path: '/feedback', component: () => import('src/pages/FeedBack.vue') },
->>>>>>> Stashed changes
       { path: '/help&support', component: () => import('pages/HelpAndSupport.vue') },
       { path: '/login', component: () => import('pages/LoginPage.vue') },
       { path: '/homepage', component: () => import('pages/HomePage.vue') },
       { path: '/plannerhomepage', component: () => import('pages/planner_Home.vue')},
       { path: '/dee', component: () => import('pages/DeePage.vue') },
       { path: '/busticketbookingpage', component: () => import('pages/BusTicketBooking_3rdPage.vue')},
-<<<<<<< Updated upstream
       { path: '/schedular-assign-crew', component: () => import('pages/Sch_AssignCrew.vue')},
       { path: '/schedular-available-crew', component: () => import('pages/Sch_AvailableCrew.vue')},
       { path: '/schedular-manage-crew', component: () => import('pages/Sch_ManageCrew.vue')},
       { path: '/schedular-crew-details', component: () => import('pages/Sch_crewDetails.vue')},
-      { path: '/Paymentpage', component: () => import('pages/PaymentPage.vue')},
+      { path: '/PaymentPage', component: () => import('pages/PaymentPage.vue')},
       { path: '/paymentgateway', component: () => import ('pages/PaymentGateway.vue')},
-=======
-      {path:'/PaymentPage',component:()=> import('pages/PaymentPage.vue')}
->>>>>>> Stashed changes
     ]
   },
 
